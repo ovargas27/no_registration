eefd8c402471cb1bc4847887e310efd945acd7b6
Omar Vargas <ovargas27@gmail.com>
2011-07-07 09:35:19 -0500

    update keys

--- config/initializers/omnisocial.rb
+++ config/initializers/omnisocial.rb
@@ -4,7 +4,7 @@ Omnisocial.setup do |config|
   config.twitter 'JEYE1U1Ylqa348TGBoKnw', 'rDFFbAFc8Oa3vVEosy8UaWK2pZipXMFUEW4WXHNM28'
   
   # ==> Facebook
-  config.facebook '160637017343198', 'dd85d8518dc3dd3ebd2dc59dfdd61ca7', :scope => 'publish_stream'
+  # config.facebook '71e0310c960a17bd428e83ae627262fe', 'dd85d8518dc3dd3ebd2dc59dfdd61ca7', :scope => 'publish_stream'
   
   if Rails.env.production?
     
